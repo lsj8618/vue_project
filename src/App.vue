e3fb7836aadf5077028cde468bd8272d74b08afb
lsj8618 <lsj86182402@gmail.com>
2024-01-12 15:35:46 +0900

    해더 만드는 중

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <img class="img-asd" src="@/assets/13.png" alt="" />
+  <headerView class="header" />
   <router-view />
 </template>
+<script>
+import HeaderView from "./components/HeaderView.vue";
+export default {
+  components: { HeaderView },
+};
+</script>
 
 <style>
+.header {
+  position: fixed; /* 헤더를 고정 */
+  top: 0; /* 상단에 위치 */
+  /* width: 100%; */
+  opacity: 0.9;
+}
 #app {
+  max-width: 100vw;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -17,6 +28,15 @@
 
 nav {
   padding: 30px;
+}
+
+.img-asd {
+  width: 1920px;
+  height: 1009px;
+}
+
+body {
+  margin: 0 !important;
 }
 
 nav a {
---
+++ src/components/HeaderView.vue
@@ -0,0 +1,93 @@
+<template>
+  <div style="min-width: 100%">
+    <div @mouseover="isHover = false" v-if="isHover" class="header-class">
+      <div class="logo">
+        <img src="../assets/deuNavLogo.png" alt="132" />
+      </div>
+      <div class="header-sub-class" v-for="i in 6" :key="i">
+        <span class="header-text">사업단 소개</span>
+      </div>
+    </div>
+    <div @mouseleave="isHover = true" v-else class="header-class-2">
+      <div
+        class="header-sub-class-2"
+        v-for="i in 6"
+        :key="i"
+        @mouseover="isActive = i"
+      >
+        <div class="header-text-2" :class="{ isActive: isActive === i }">
+          사업단 소개
+        </div>
+        <div
+          style="margin-top: 1rem; width: 100%"
+          :class="{ isActive2: isActive === i }"
+        >
+          <div
+            style="margin-top: 1rem"
+            v-for="i in 3"
+            :key="i"
+            class="header-sub-text"
+            @click="$router.push({ path: '/about' })"
+          >
+            인사말{{ i }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isHover: false,
+      isActive: -1,
+    };
+  },
+};
+</script>
+<style scoped>
+.header-class {
+  display: flex;
+  justify-content: center;
+  padding: 2rem 10rem;
+  background-color: rgb(224, 211, 211);
+}
+.header-sub-class {
+  padding: 0 30px;
+  min-width: max-content;
+  font-size: 1.25rem;
+}
+.header-text:hover {
+  color: blue;
+}
+.isActive {
+  position: relative;
+  border-bottom: 1px solid #aaa;
+}
+
+.header-class-2 {
+  display: flex;
+  justify-content: center;
+  padding: 2rem 10rem;
+  background-color: white;
+}
+.header-sub-class-2 {
+  padding: 0 30px;
+  min-width: max-content;
+  font-size: 1.25rem;
+}
+.header-text-2:hover {
+  color: blue;
+}
+.header-sub-text:hover {
+  color: blue;
+}
+.isActive2 {
+  background-color: rgb(234, 229, 229);
+}
+.logo > img {
+  width: 293px;
+  height: 63px;
+}
+</style>
